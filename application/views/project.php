536ae86c07bf4912af0b130e19d987facfe3161b
Cameron Edwards <cameronedwards145@gmail.com>
2015-09-27 16:28:15 +0100

    Changed header images to be responsive

--- application/views/project.php
+++ application/views/project.php
@@ -1,4 +1,5 @@
-<?php echo img(array('src' =>'assets/img/{headerImage}')); ?>
+<?php echo img(array('src' =>'assets/img/{headerImage}',
+                     'class' => 'img-responsive')); ?>
 
 <style>
 h1, h2, h3
